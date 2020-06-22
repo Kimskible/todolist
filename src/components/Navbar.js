1542fe9f1a3b46461d9446f9db82a60266f455da
Kimski <k_oksana15@hotmail.com>
2020-06-21 23:47:21 -0500

    psa

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return(
+    <div className="">
+      <h1>Hello</h1>
+      <h3> My first react app</h3>
+      <p>This is a simple "todolist" I'll be doing</p>
+      <h3> to be continued..</h3>
+    </div>
+  )
+
+}
+
+export default Navbar;
