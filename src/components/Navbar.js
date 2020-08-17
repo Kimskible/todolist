f117a0898e6d43c452c761b88ebfdf71dd0f767e
Kimberly Stewart <ks039495@digicelgroup.com>
2020-08-17 12:01:39 -0500

    refactor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,6 @@ function Navbar() {
     <div className="">
       <h1>Hello</h1>
       <h3> My first react app</h3>
-      <p>This is a simple "todolist" I'll be doing</p>
       <h3> to be continued..</h3>
     </div>
   )
